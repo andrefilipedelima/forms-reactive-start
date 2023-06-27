5b4a59423a938c8434a3175c6da0ce55144fca28
André de Lima <andrefilipedelima@hotmail.com>
2023-06-27 14:58:38 -0300

    estudo sobre reactive forms

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
